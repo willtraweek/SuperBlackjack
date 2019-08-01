41478726d6798ff9adf367709c0ebd69a8a233ad
Will Traweek <will.traweek@gmail.com>
2019-07-31 22:17:03 -0500

    Add Main Controller

---
+++ SuperBlackjack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Controls/Deck.h"
+#include "Controls/Die.h"
+#include "Controllers/Player.h"
+
+using namespace std;
+
+int main(){
+	Deck deck = Deck();
+
+	cout << endl;
+	deck.draw().print();
+
+	Die die = Die();
+	die.roll();
+	die.print();
+}
