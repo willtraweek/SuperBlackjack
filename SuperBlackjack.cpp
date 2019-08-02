0d0792a55b44af41955bb18c30520cc0f8e442bd
Will Traweek <will.traweek@gmail.com>
2019-08-02 10:50:40 -0500

    Add scrubbing for invalid inputs on play loop

--- SuperBlackjack.cpp
+++ SuperBlackjack.cpp
@@ -1,6 +1,6 @@
 #include "Controls/Deck.h"
 #include "Controls/Die.h"
-#include "Controllers/Player.h"
+#include "Controllers/Table.h"
 #include <vector>
 
 using namespace std;
@@ -11,17 +11,22 @@ int main(){
 	int handNumber = 1;
 	char playerMove = 'l';
 
+	Table * table = new Table();
+
 	while(playerMove != 'q'){
-		cout << "--- HAND " << handNumber << " ----";
+		cout << "--- HAND " << handNumber << " ----\n";
 
-		Player * player = new Player();
+		table -> SetBet();
 
-		player -> SetBet();
+		//TODO: ADD PlayerTurn() where Player can choose what to draw,roll, or stay on.
 
 		cout << "Would you like to play again?\nPlease type 'q' to quit or any other letter to continue.\n";
+		cin.clear();
+		cin.ignore(1000, '\n');
 		cin >> playerMove;
+		
 		handNumber++;
 
-		delete player;
 	}
+	delete table;
 }
